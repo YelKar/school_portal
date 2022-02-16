fa556bb4341e070ac4e0ae383bfb48fae054cf3f
YelKar <karamyshev_e@1060.ru>
2022-02-16 21:13:15 +0300

    sqlalchemy

--- app/views.py
+++ app/views.py
@@ -1,8 +1,30 @@
-from app import app
-from flask import render_template, url_for
+from app import app, User, db
+from flask import render_template, url_for, request, flash
+
+
+base = "base.html"
 
 
 @app.route('/')
 def index():
     title = "1060"
-    return render_template("index.html", title=title, base="base.html")
+    return render_template("index.html", title=title, base=base)
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form_view():
+    if request.method == 'POST':
+        form = dict(request.form)
+        username = form["username"]
+        password = form["password"]
+        print(username, password)
+
+        user = User(username=username, password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash("OK", category="some")
+
+        except:
+            flash("error", category="some")
+    return render_template("form.html", base=base)
