a821be5965508e968b2046659bf4cbb35f1ce7fb
YelKar <karamyshev_e@1060.ru>
2022-02-24 21:49:04 +0300

    add cursor

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,10 +1,13 @@
 function change_themes() {
     document.querySelector("html").classList.toggle("light-theme")
-    document.querySelector("html").classList.toggle("dark-theme")
+    let theme = document.querySelector("html").classList.toggle("dark-theme") ? "dark-theme" : "light-theme"
+    document.cookie = `theme = ${theme}`
 }
 
 function open_navbar() {
-    document.querySelector('.navbar').classList.toggle('open')
+    let is_open = document.querySelector('.navbar').classList.toggle('open') ? "open" : ""
+    document.cookie = `navbar_is_open = ${is_open}`
 }
+
 
 
