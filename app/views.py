f7f05b06e8ba366aa1be2b6e2b5d2f1b9ee4c2f0
YelKar <karamyshev_e@1060.ru>
2022-02-24 08:55:10 +0300

    creating error pages

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 from app import app
-from flask import render_template, url_for, request, flash
+from flask import render_template, url_for, \
+    request, flash, abort
+from werkzeug.exceptions import HTTPException
 
 
 base = "base.html"
 
 
 @app.route('/')
-@app.route('/index')
-@app.route('/main')
 def index():
     title = "1060"
     return render_template("index.html", title=title, base=base)
@@ -16,3 +16,18 @@ def index():
 @app.route("/print")
 def print_document():
     return render_template("print.html", base=base)
+
+
+@app.route("/abort-<int:error_code>")
+def abort_error(error_code):
+    abort(error_code)
+    return f"{error_code}"
+
+
+@app.errorhandler(HTTPException)
+def error_requests(e: HTTPException):
+    return render_template("errors.html", base=base,
+                           error=e, title=e.name), e.code
+
+
+from . import form_views
