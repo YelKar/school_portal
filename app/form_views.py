605d0cc217dabdb5ba447501df7278c923d63c61
YelKar <karamyshev_e@1060.ru>
2022-03-24 08:03:32 +0300

    adding doc-string

--- app/form_views.py
+++ app/form_views.py
@@ -6,33 +6,44 @@ from .views import base
 from flask import render_template, url_for, session, redirect, request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
-from app.database import Users
+from app.database import Users, UserInfo
 from app.forms import *
 from colorama import Fore, Style
 from time import strftime
 
 
-login_manager.login_view = "login"
-login_manager.login_message = "Вы не авторизованы"
-
-
 @login_manager.user_loader
-def load_user(user_id):
+def load_user(user_id) -> UserLogin:
     return UserLogin().from_db(user_id)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """Add user to db
+
+    render form from RegisterForm object
+
+    get form data if form passed validation, generate password hash and add data to the DataBase
+    :return: str (HTML_Template)
+    """
     form = RegisterForm()   # create form object
     if form.validate_on_submit():    # if form passed validation
         # coding password
         password_hash = generate_password_hash(form.password.data)
         # get data and create row for database
-        u = Users(username=form.username.data, password=password_hash,
-                  email=form.email.data,
-                  firstname=form.firstname.data, lastname=form.lastname.data, patronymic=form.patronymic.data, classroom=str(form.classroom.data)+form.classletter.data)
+        u = Users(
+            username=form.username.data, password=password_hash,
+            email=form.email.data,
+            firstname=form.firstname.data, lastname=form.lastname.data, patronymic=form.patronymic.data,
+            classroom=str(form.classroom.data) + form.classletter.data
+        )
+        ui = UserInfo(
+            user_id=u.id,
+            sex=form.sex.data,
+        )
         # add row to database
         db.session.add(u)
+        db.session.add(ui)
         db.session.flush()
         db.session.commit()
         return redirect(url_for("login"))
@@ -41,10 +52,19 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """Log in user
+
+    if user is logged in, he is redirected to the profile
+
+    if user is not logged, render the form from RegisterForm object
+    after filling out the form, authorized user
+
+    :return: str (HTML_Template)
+    """
     if current_user.is_authenticated:  # if user is authorized, he is redirected to the profile
         return redirect(url_for("profile"))
     else:
-        form = LoginForm()   # create form object
+        form: FlaskForm = LoginForm()   # create form object
         if form.validate_on_submit():   # if form passed validation
             user: Users = Users.query.filter(form.username.data == Users.username).first()  # get user from database
             user_login = UserLogin().create(user)
--- app/views.py
+++ app/views.py
@@ -6,51 +6,106 @@ from flask_login import login_required, current_user
 from app.database import Users
 
 
-base = "base.html"
+base = "base.html"  # Путь к базе данных
 
 
+# main page
 @app.route('/')
-def index():
-    title = "1060"
-    return render_template("index.html", title=title, base=base)
+def index() -> str:
+    """
+    render html template from "index.html"
+    :return: str
+    """
+    return render_template(
+        "index.html",
+        title="1060",
+        base=base
+    )
 
 
 @app.route("/abort-<int:error_code>")
-def abort_error(error_code):
+def abort_error(error_code: int) -> None:
+    """
+    cause <response error_code> (400 <= error_code <= 505)
+    :param error_code: int
+    :return: None
+    """
     abort(error_code)
-    return f"{error_code}"
 
 
 @app.route("/profile")
 @login_required
 def profile():
+    """
+    show profile page if user is authorize
+    else redirect to <login_manager.login_view> and show message <login_manager.login_message>
+    :return: str (HTML_Template)
+    """
     return render_template("profile/profile.html", base=base)
 
 
 @app.route('/profile/info')
+@login_required
 def profile_info():
     return render_template("profile/profile_info.html", base=base)
 
 
 @app.route('/chose/documents')
 def chose_documents():
-    return render_template("documents/chose_documents.html", base=base, users=Users, db=db)
+    """chose document to print
+
+    if request method is POST goto chose_student
+    else render form for chose document
+    TODO создать форму для выбора документов
+    :return: str (HTML_Template)
+    """
+    return render_template(
+        "documents/chose_documents.html",
+        base=base,
+        users=Users,
+        db=db
+    )
 
 
-@app.route('/chose/students')
+@app.route('/chose/students', methods=['GET', 'POST'])
 def chose_students():
-    return render_template("documents/chose_students.html", base=base, users=Users, db=db)
+    """chose students and goto print
+
+    if request method is POST goto print
+    else render form for chose students
+    TODO Реализовать получение данных о выбранных пользователях
+    TODO и переброску их на страницу печати
+    TODO реализовать фильтры
+    :return: str (HTML_Template)
+    """
+    if request.method == "POST":
+        print(request.form)
+    return render_template(
+        "documents/chose_students.html",
+        base=base,
+        users=Users,
+        db=db
+    )
 
 
 @app.route("/print")
 def print_document():
-    return render_template("documents/print.html", base=base)
+    return render_template("documents/print.html", base=base, Users=Users)
 
 
 @app.errorhandler(HTTPException)
 def error_requests(e: HTTPException):
-    return render_template("errors.html", base=base,
-                           error=e, title=e.name), e.code
+    """
+    catches error responses and shows its page
+    :param e: HTTPException
+    :return: str (HTML_Template)
+    """
+    return render_template(
+        "errors.html",
+        base=base,
+        error=e,
+        title=e.name
+    ), e.code
 
 
 @app.route('/docs-<name>')
