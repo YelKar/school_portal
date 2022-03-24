1df2af0935ba736f7ededaf6272b50dd2bbf164f
YelKar <karamyshev_e@1060.ru>
2022-03-24 08:00:26 +0300

    adding datetime filters to jinja

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,29 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
+import datetime
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message = "Вы не авторизованы"
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+
+@app.template_filter("fromTimestamp")
+def from_timestamp_filter(product):
+    return datetime.datetime.fromtimestamp(product)
+
+
+@app.template_filter("strftime")
+def strftime_filter(product: datetime.datetime, form: str):
+    return product.strftime(form)
 
 
 from app import database, views
