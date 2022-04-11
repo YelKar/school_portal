5c4111d5ea991aa735d10f5b64a65a9e0ec87feb
YelKar <karamyshev_e@1060.ru>
2022-04-11 11:03:29 +0300

    Upgraded register form

--- app/forms.py
+++ app/forms.py
@@ -60,16 +60,35 @@ class RegisterForm(FlaskForm):
 
     def validate_email(self, field) -> ValidationError or None:
         """checking if email is used"""
-        emails = [user.email for user in Users.query.all()]
-        if field.data in emails:
+        emails = [user.email.lower() if user.email else user.email for user in Users.query.all()]
+        if field.data.lower() in emails:
             raise ValidationError("Такой email уже зарегистрирован")
+
+    def validate_firstname(self, field: StringField):
+        if not field.data.isalpha():
+            raise ValidationError("Имя должно состоять из букв")
+        elif not (field.data[0].isupper() and field.data[1:].islower()):
+            raise ValidationError("Имя должно начинаться с большой буквы")
+
+    def validate_lastname(self, field: StringField):
+        if not field.data.isalpha():
+            raise ValidationError("Фамилия должна состоять из букв")
+        elif not (field.data[0].isupper() and field.data[1:].islower()):
+            raise ValidationError("Фамилия должна начинаться с большой буквы")
+
+    def validate_patronymic(self, field: StringField):
+        if not field.data.isalpha():
+            raise ValidationError("Отчество должно состоять из букв")
+        elif not (field.data[0].isupper() and field.data[1:].islower()):
+            raise ValidationError("Отчество должно начинаться с большой буквы")
 
     username = StringField("Логин: *", validators=[DataRequired(), Length(min=2, max=30)],
                            render_kw={"placeholder": "Ivanious"})
-    password = PasswordField("Пароль: *", validators=[DataRequired(), Length(min=6)],
+    password = PasswordField("Пароль: *", validators=[DataRequired(), Length(min=6),
+                                                      EqualTo("repeat_password", "Пароли не равны")],
                              render_kw={"placeholder": "••••••"})
     repeat_password = PasswordField("Повторите пароль: *",
-                                    validators=[DataRequired(), EqualTo("password", "Пароли не равны")],
+                                    validators=[DataRequired()],
                                     render_kw={"placeholder": "••••••"})
 
     email = EmailField("Email", validators=[Email(), DataRequired()],
