bdd6fa9b9b96fe16546d25fd27d38b207f0308df
Student <karamyshev_e@1060.ru>
2022-04-21 15:06:25 +0300

    added script for installing all libraries

---
+++ install_libraries.bat
@@ -0,0 +1,9 @@
+cd venv\Scripts
+
+pip install flask
+pip install flask_login
+pip install flask_sqlalchemy
+pip install flask_wtf
+pip install email-validator
+pip install flask_admin
+pip install flask_migrate
